1bd419fef849eb171d773fa74406324aeae58697
Lukas Riemenschneider <lukas.riemenschneider@hs-bochum.de>
2015-12-13 22:15:37 +0100

    Add (first) hash specialization

--- include/vec.hpp
+++ include/vec.hpp
@@ -2,9 +2,25 @@
 #include "mat.hpp"
 
 #include <cmath>
+#include <functional>
 
 template <typename Type, std::size_t M>
 using vector = matrix<Type, M, 1>;
+
+namespace std
+{
+	template<typename Type>
+	struct hash<matrix<Type, 2, 1>>
+	{
+		typedef matrix<Type, 2, 1> argument_type;
+		typedef std::size_t result_type;
+		
+		result_type operator()(const argument_type& v) const
+		{
+			return v.x + (v.y * Type(7));
+		}	
+	};
+}
 
 template <typename Type, std::size_t M>
 vector<Type, M> normalize(const vector<Type, M>& v);
